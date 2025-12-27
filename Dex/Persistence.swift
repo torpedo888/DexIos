4207144a30af78ee005ce97178c93c1132f92ed6
Batorligeti Ferenc (Contractor) <ferenc.batorligeti@capgemini.com>
2025-12-27 16:56:18 +0100

    all item code replaced with pokemon code

--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -55,6 +55,7 @@ struct PersistenceController {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
